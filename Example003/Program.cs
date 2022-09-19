2757e9bd8234898c88a7be56e4897b872afe299f
timur <yusupov-rr22@mail.ru>
2022-09-19 19:33:45 +0700

    hello user

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,6 +1,4 @@
-﻿int numberA = new Random(). Next(1,10); //1 2 3...10
-Console.WriteLine(numberA);
-int numberB = new Random(). Next(1,10);
-Console.WriteLine(numberB);
+﻿int numberA = 3;
+int numberB = 5;
 int result = numberA + numberB;
 Console.WriteLine(result);
